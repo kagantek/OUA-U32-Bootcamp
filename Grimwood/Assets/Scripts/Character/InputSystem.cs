49092eb95b8ca969300c4e703ec3921c57a2766b
Kagan Tek <kagantek2003@hotmail.com>
2024-07-23 02:11:37 +0300

    Camera Controller

--- Grimwood/Assets/Scripts/Character/InputSystem.cs
+++ Grimwood/Assets/Scripts/Character/InputSystem.cs
@@ -18,14 +18,25 @@ public class InputSystem : MonoBehaviour
 
     [SerializeField]
     public InputSettings input;
+    
+    
+    [Header("Camera & Character Syncing")]
+    public float lookDistance = 5;
+    public float lookSpeed = 5;
+
+    Transform camCenter;
 
     void Start()
     {
         moveScript = GetComponent<Movement>();
+        camCenter = Camera.main.transform.parent;
     }
 
     void Update()
     {
+        if (Input.GetAxis(input.forwardInput) != 0 || Input.GetAxis(input.strafeInput) != 0)
+            RotateToCamView();
+        
         moveScript.AnimateCharacter(Input.GetAxis(input.forwardInput), Input.GetAxis(input.strafeInput));
         moveScript.SprintCharacter(Input.GetButton(input.sprintInput));
 
@@ -33,5 +44,20 @@ public class InputSystem : MonoBehaviour
         {
             moveScript.JumpCharacter();
         }
+    }
+
+    void RotateToCamView()
+    {
+        Vector3 camCenterPos = camCenter.position;
+
+        Vector3 lookPoint = camCenterPos + (camCenter.forward * lookDistance);
+        Vector3 direction = lookPoint - transform.position;
+
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        lookRotation.x = 0;
+        lookRotation.z = 0;
+
+        Quaternion finalRotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * lookSpeed);
+        transform.rotation = finalRotation;
     }
 }
