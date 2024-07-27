17b7da940fc6992bc914528d61209b7935081524
Kerem Aslan URAL <smilodonkerem@gmail.com>
2024-07-28 02:50:22 +0300

    Menu and Collider Update
    -Main menu updated
    -Credits panel filled
    -Collider bugs fixed

---
+++ Grimwood/Assets/Scripts/GameMenu/OpenLink.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void OpenURLKerem()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/kerem-aslan-ural/");
+    }
+    public void OpenURLIsrafil()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/israfil-ercan-a427ba238/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app");
+    }
+    public void OpenURLEylul()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/iremeyluloztek/");
+    }
+    public void OpenURLKagan()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/kagan-tek/");
+    }
+}
